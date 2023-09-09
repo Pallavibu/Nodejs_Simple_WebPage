40322644e37b381faa9a704171a6e89c5113ebc7
Pallavi <pallavibu44@gmail.com>
2023-09-09 18:25:56 +0530

    working on file

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-console.log("pallavi")
+// console.log("pallavi")
 
-console.log("bpallavi")
+// console.log("bpallavi")
+
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const app = http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url) 
+
+    let conteType = 'text/html'
+
+    let ext = path.extname(filePath)
+    if(!ext) {
+      filePath += '.html' 
+    }
+
+    switch(ext) {
+        case '.css': 
+            contentType = 'text/css'
+            break
+        case '.js': 
+            contentType = 'text/javascript'
+            break
+        default: 
+            contentType = 'text/html' 
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if(err) {
+                    res.writeHead(500)
+                    res.end('Error!!!')  
+                } else {
+                    res.writeHead(404, {
+                       'Content-Type': contentType 
+                    })
+                    res.end(data)
+                }
+                
+            })
+        } else {
+            res.writeHead(200, {
+                'Content-Type': contentType 
+            })
+            res.end(content)
+        }
+        
+
+    })
+    
+})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
--- module.js
+++ module.js
@@ -1,1 +1,9 @@
 
+fs.readFile(path.join(__dirname, 'test', 'test.txt'), 'utf-8', (err, data) => {
+    if(err) {
+        throw err
+    }
+  
+
+    console.log(data)
+})
